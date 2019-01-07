205c3bead880a685b6a46ae6c85a5fa45f9f26d8
pofystyy <pofystyy@gmail.com>
2019-01-07 21:46:07 +0200

    add 2 solutions in 7 kyu

---
+++ 7 kyu/exes_and_ohs.rb
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/55908aad6620c066bc00002a
+
+# Details:
+
+# Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+# Examples input/output:
+
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
+
+def XO(str)
+  str.downcase.count('x') == str.downcase.count('o')
+end
