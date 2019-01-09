c4bd51bbf1f90f793b26c826d6c3f58b4fb2f284
pofystyy <pofystyy@gmail.com>
2019-01-09 14:20:13 +0200

    add 2 solutions in 7 kyu

---
+++ 7 kyu/bingo.rb
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/5a1ee4dfffe75f0fcb000145
+
+# Details:
+
+# For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+# Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array). Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+
+BINGO = [2, 9, 14, 7, 15]
+
+def bingo(arr)
+  (arr & BINGO).sort == BINGO.sort ? 'WIN' : 'LOSE'
+end
