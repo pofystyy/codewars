b1acd4a3fb18fab0e8c23616aa55fda4ad16ec5f
pofystyy <pofystyy@gmail.com>
2019-01-12 20:04:42 +0200

    add 2 solutions in 6 kyu

---
+++ 6 kyu/find_the_missing_letter.rb
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/5839edaa6754d6fec10000a2
+
+# Details:
+
+# Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+# You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+# The array will always contain letters in only one case.
+
+# Example:
+
+# ['a','b','c','d','f'] -> 'e'
+# ['O','Q','R','S'] -> 'P'
+
+# (Use the English alphabet with 26 letters!)
+
+# Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+# I have also created other katas. Take a look if you enjoyed this kata!
+
+def find_missing_letter(arr)
+  ((arr[0]..arr[-1]).to_a - arr).join
+end
---
+++ 6 kyu/multiples_of_3_or_5.rb
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/514b92a657cdc65150000006
+
+# Details:
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+#     Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(num)
+   (1...num).select { |i| i%3 == 0 || i%5 == 0 }.sum
+end
