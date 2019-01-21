8ba71bc1e33c14055414aaa8143bc216d372c3fe
pofystyy <pofystyy@gmail.com>
2019-01-21 13:49:30 +0200

    add solution in 5 kyu

---
+++ 5 kyu/phone_my_kids.rb
@@ -0,0 +1,34 @@
+# https://www.codewars.com/kata/5200165c39ae4c612200027b
+
+# Details:
+
+# Sometimes you need to know all classes that inherited from you, and to be able to do simple things like listing or calling all of them.
+
+# Write a class called "Mother" that tracks all inheritances that are done to her (and her kids), together with a class method "phone_kids" that calls in turn the class method "phone" of all kids.
+
+# So if you have two kids
+
+# class Foo < Mother
+# end
+
+# class Bar < Mother
+# end
+
+# The line
+
+# Mother.phone_kids
+
+# Would call (in either order)
+
+# Foo.phone
+# Bar.phone
+
+class Mother
+  def self.descendants
+    ObjectSpace.each_object(Class).select { |klass| klass < self }
+  end
+
+  def self.phone_kids
+    self.descendants.map(&:phone)
+  end
+end
