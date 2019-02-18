646be1d12b6400ef706bda37cd51392cf87813ab
pofystyy <tanya.romas@gmail.com>
2019-02-18 20:27:42 +0200

    add solution in 6 kyu

---
+++ 6 kyu/find_the_missing_term_in_an_arithmetic_progression.rb
@@ -0,0 +1,29 @@
+# https://www.codewars.com/kata/52de553ebb55d1fca3000371
+
+# Details:
+
+# An Arithmetic Progression is defined as one in which there is a constant difference between 
+# the consecutive terms of a given series of numbers. You are provided with consecutive elements 
+# of an Arithmetic Progression. There is however one hitch: exactly one term from the original 
+# series is missing from the set of numbers which have been given to you. The rest of the given 
+# series is the same as the original AP. Find the missing term.
+
+# You have to write the function findMissing(list), list will always be at least 3 numbers. 
+# The missing term will never be the first or last one.
+# Example
+
+# findMissing([1, 3, 5, 9, 11]) == 7
+
+# ```
+
+# PS: This is a sample question of the facebook engineer challenge on interviewstreet. I found 
+# it quite fun to solve on paper using math, derive the algo that way. Have fun!
+
+def find_missing(sequence)
+  step = sequence[1] - sequence[0]
+  if sequence[0] < sequence[1]
+    ((sequence[0]..sequence[-1]).step(step.abs).to_a - sequence).join.to_i
+  else
+    ((sequence[-1]..sequence[0]).step(step.abs).to_a.reverse - sequence).join.to_i
+  end
+end
