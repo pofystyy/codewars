7ddc6c98edcb680c0df053112edb519afd65cfec
pofystyy <tanya.romas@gmail.com>
2019-03-13 17:02:01 +0200

    add solution in 7 kyu

---
+++ 7 kyu/simple_fun_#144:_distinct_digit_year.rb
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/58aa68605aab54a26c0001a6
+
+# Details:
+
+# Task
+
+# The year of 2013 is the first year after the old 1987 with only distinct digits.
+
+# Now your task is to solve the following problem: given a year number, find the minimum year number which 
+# is strictly larger than the given one and has only distinct digits.
+# Input/Output
+
+#     [input] integer year
+
+#     1000 ≤ year ≤ 9000
+
+#     [output] an integer
+
+#     the minimum year number that is strictly larger than the input number year and all it's digits are 
+#     distinct.
+
+def distinct_digit_year(year)
+  year = year + 1
+  return year if year.digits.map { |i| p year.digits.count(i) == 1 }.all?
+  distinct_digit_year(year)
+end
