d1f82b8b358e453f4cffd7b5e74472f2abd743bf
pofystyy <pofystyy@gmail.com>
2019-04-07 17:30:20 +0300

    add solution in 7 kyu

---
+++ 7 kyu/split_in_parts.rb
@@ -0,0 +1,24 @@
+# https://www.codewars.com/kata/5650ab06d11d675371000003
+
+# Details:
+
+# The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+
+# Example:
+
+# Split the below string into other strings of size #3
+
+# 'supercalifragilisticexpialidocious'
+
+# Will return a new string
+# 'sup erc ali fra gil ist ice xpi ali doc iou s'
+
+# Assumptions:
+
+# String length is always greater than 0
+# String has no spaces
+# Size is always positive
+
+def split_in_parts (s, part_length)
+  s.gsub(/(\w{#{part_length}})/, '\1 ').strip
+end
