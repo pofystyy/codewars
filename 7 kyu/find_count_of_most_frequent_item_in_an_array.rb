2c841431e97c8a3ff2ebf105b3af69cbf7a6cc1a
pofystyy <pofystyy@gmail.com>
2019-04-14 17:00:30 +0300

    add solution in 7 kyu

---
+++ 7 kyu/find_count_of_most_frequent_item_in_an_array.rb
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/56582133c932d8239900002e
+
+# Details:
+
+# Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return 0
+# Example
+
+# input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+# ouptut: 5
+
+# The most frequent number in the array is -1 and it occurs 5 times.
+
+def most_frequent_item_count(collection)
+  collection.group_by { |i| i }.values.map(&:count).max || 0
+end
