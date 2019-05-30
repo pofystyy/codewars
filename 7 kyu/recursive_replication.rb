3ad24e05b0eda2887c292a40f9d8e8dde9762d9a
pofystyy <tanya.romas@gmail.com>
2019-05-30 20:01:44 +0300

    add solution in 7 kyu

---
+++ 7 kyu/recursive_replication.rb
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/57547f9182655569ab0008c4
+
+# Details:
+
+# You need to design a recursive function called replicate which will receive arguments times and number.
+
+# The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+
+# As tempting as it may seem, do not use loops to solve this problem.
+
+def replicate(times, number)
+  Array.new(times, number) rescue []
+end
